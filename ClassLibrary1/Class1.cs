39183b75977aa4fa66b0abc11ce52c980d45269a
Artur Siwiak <artur.siwiak@gmail.com>
2023-11-20 22:03:27 +0100

    init

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1;
+public class Class1
+{
+    private readonly IServiceCollection _serviceCollection; // Ctrl + . doesn't suggest any nuget packages 
+    private readonly IMediatr _mediator; // Ctrl + . doesn't suggest any nuget packages
+    private readonly AbstractValidator _abstractValidator; // same as above
+    private readonly IConfiguration _configuration; // same as above
+
+    // all other items after pressing Ctrl + . are displayed correctly - like: generate type, disable spelling and so one
+}
